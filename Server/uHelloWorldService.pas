104b0e3e4903600151a02a1af1dc732c88c05db9
Jens Fudge <jens.fudge@archersoft.dk>
2025-03-12 15:46:08 +0100

    Added Login Service (but nothing in it)

--- Server/uHelloWorldService.pas
+++ Server/uHelloWorldService.pas
@@ -9,7 +9,8 @@ type
   [ServiceContract]
   IHelloWorldService = interface(IInvokable)
     ['{BDD66CED-4261-48C6-84CC-39453AC90985}']
-    [HttpGet] function Sum(A, B: double): double;
+    [HttpGet]
+    function Sum(A, B: double): double;
     // By default, any service operation responds to (is invoked by) a POST request from the client.
     function EchoString(Value: string): string;
   end;
